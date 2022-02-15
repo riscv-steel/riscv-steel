71de41a6cf73446f30cd40b21fc1dcdf631ee232
Rafael Calcada <rafaelcalcada@hotmail.com>
2022-02-14 18:50:07 -0600

    Revert "Fix issue #4: Trap address in Vector Mode"
    This reverts commit 76263d9bfff83ddf67951efcdd6849b5b1bd0acc.

--- steel_core.v
+++ steel_core.v
@@ -1092,7 +1092,7 @@ module csr_file(
     wire [31:0] vec_mux_out;
     wire [31:0] base_offset;
     wire [31:0] mtvec_reset_value = `MTVEC_RESET;
-    assign base_offset = CAUSE_IN << 2;
+    assign base_offset = cause << 2;
     assign trap_mux_out = int_or_exc ? vec_mux_out : {mtvec_base, 2'b00};
     assign vec_mux_out = mtvec[0] ? {mtvec_base, 2'b00} + base_offset : {mtvec_base, 2'b00};
     assign TRAP_ADDRESS = trap_mux_out;
