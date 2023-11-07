c5a609c2bb2c11a6e63193b7fa958de70e72d4e1
Rafael Calcada <rafaelcalcada@hotmail.com>
2023-11-07 23:08:15 +0100

    Update copyright notice.

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) Rafael Calcada
+Copyright (c) RISC-V Steel Project
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- hardware/tests/rvsteel-core-unit-tests.v
+++ hardware/tests/rvsteel-core-unit-tests.v
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) Rafael Calcada
+Copyright (c) RISC-V Steel Project
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
