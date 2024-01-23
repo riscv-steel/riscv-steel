41789ffe2f0d4c2b90001fc862e09414ee032df2
Rafael Calcada <rafaelcalcada@gmail.com>
2024-01-23 18:48:34 +0100

    Adopt the new system_bus in the SoC IP design

--- ip/soc/rvsteel_soc.v
+++ ip/soc/rvsteel_soc.v
@@ -38,17 +38,13 @@ Top Module:    rvsteel_soc
 module rvsteel_soc #(
 
   // Frequency of 'clock' signal
-  parameter CLOCK_FREQUENCY = 50000000,
-
+  parameter CLOCK_FREQUENCY = 50000000  ,
   // Desired baud rate for UART unit
-  parameter UART_BAUD_RATE = 9600,
-
-  // Memory size in bytes - must be a multiple of 32
-  parameter MEMORY_SIZE = 8192,
-
+  parameter UART_BAUD_RATE = 9600       ,
+  // Memory size in bytes - must be a power of 2
+  parameter MEMORY_SIZE = 8192          ,
   // Text file with program and data (one hex value per line)
-  parameter MEMORY_INIT_FILE = "",
-
+  parameter MEMORY_INIT_FILE = ""       ,
   // Address of the first instruction to fetch from memory
   parameter BOOT_ADDRESS = 32'h00000000
 
@@ -62,67 +58,47 @@ module rvsteel_soc #(
 
   );
 
+  // Interrupts
+
   wire          irq_external;
   wire          irq_external_response;
 
+  // System bus configuration
+
+  localparam NUM_DEVICES    = 2;
+  localparam D0_RAM         = 0;
+  localparam D1_UART        = 1;
+
+  wire  [NUM_DEVICES*32-1:0] device_base_address;     
+  wire  [NUM_DEVICES*32-1:0] device_mask_address;
+
+  assign device_base_address[32*D0_RAM  +: 32]  = 32'h0000_0000;
+  assign device_mask_address[32*D0_RAM  +: 32]  = 32'hFFFF_E000;
+
+  assign device_base_address[32*D1_UART +: 32]  = 32'h8000_0000;
+  assign device_mask_address[32*D1_UART +: 32]  = 32'hFFFF_FFF8;
+
   // RISC-V Steel 32-bit Processor (Manager Device) <=> System Bus
 
-  wire  [31:0]  rw_address;
-  wire  [31:0]  read_data;
-  wire          read_request;
-  wire          read_response;
-  wire  [31:0]  write_data;
-  wire  [3:0 ]  write_strobe;
-  wire          write_request;
-  wire          write_response;
+  wire  [31:0]                manager_rw_address      ;
+  wire  [31:0]                manager_read_data       ;
+  wire                        manager_read_request    ;
+  wire                        manager_read_response   ;
+  wire  [31:0]                manager_write_data      ;
+  wire  [3:0 ]                manager_write_strobe    ;
+  wire                        manager_write_request   ;
+  wire                        manager_write_response  ;
 
-  // RAM Memory (Device #0) <=> System Bus
+  // System Bus <=> Managed Devices
 
-  wire  [31:0]  device0_rw_address;
-  wire  [31:0]  device0_read_data;
-  wire          device0_read_request;
-  wire          device0_read_response;
-  wire  [31:0]  device0_write_data;
-  wire  [3:0 ]  device0_write_strobe;
-  wire          device0_write_request;
-  wire          device0_write_response;
-
-  // UART (Device #1) <=> System Bus
-
-  wire  [31:0]  device1_rw_address;
-  wire  [31:0]  device1_read_data;
-  wire          device1_read_request;
-  wire          device1_read_response;
-  wire  [31:0]  device1_write_data;
-  wire  [3:0 ]  device1_write_strobe;
-  wire          device1_write_request;
-  wire          device1_write_response;
-
-  /* Uncomment to add new devices
-
-  // Device #2 <=> System Bus
-
-  wire  [31:0]  device2_rw_address;
-  wire  [31:0]  device2_read_data;
-  wire          device2_read_request;
-  wire          device2_read_response;
-  wire  [31:0]  device2_write_data;
-  wire  [3:0 ]  device2_write_strobe;
-  wire          device2_write_request;
-  wire          device2_write_response;
-
-  // Device #3 <=> System Bus
-
-  wire  [31:0]  device3_rw_address;
-  wire  [31:0]  device3_read_data;
-  wire          device3_read_request;
-  wire          device3_read_response;
-  wire  [31:0]  device3_write_data;
-  wire  [3:0 ]  device3_write_strobe;
-  wire          device3_write_request;
-  wire          device3_write_response;
-
-  */
+  wire  [31:0]                device_rw_address       ;
+  wire  [NUM_DEVICES*32-1:0]  device_read_data        ;
+  wire  [NUM_DEVICES-1:0]     device_read_request     ;
+  wire  [NUM_DEVICES-1:0]     device_read_response    ;
+  wire  [31:0]                device_write_data       ;
+  wire  [3:0]                 device_write_strobe     ;
+  wire  [NUM_DEVICES-1:0]     device_write_request    ;
+  wire  [NUM_DEVICES-1:0]     device_write_response   ;
 
   rvsteel_core #(
 
@@ -138,14 +114,14 @@ module rvsteel_soc #(
 
     // IO interface
 
-    .rw_address                     (rw_address                         ),
-    .read_data                      (read_data                          ),
-    .read_request                   (read_request                       ),
-    .read_response                  (read_response                      ),
-    .write_data                     (write_data                         ),
-    .write_strobe                   (write_strobe                       ),
-    .write_request                  (write_request                      ),
-    .write_response                 (write_response                     ),
+    .rw_address                     (manager_rw_address                 ),
+    .read_data                      (manager_read_data                  ),
+    .read_request                   (manager_read_request               ),
+    .read_response                  (manager_read_response              ),
+    .write_data                     (manager_write_data                 ),
+    .write_strobe                   (manager_write_strobe               ),
+    .write_request                  (manager_write_request              ),
+    .write_response                 (manager_write_response             ),
 
     // Interrupt signals (hardwire inputs to zero if unused)
 
@@ -164,83 +140,41 @@ module rvsteel_soc #(
 
   system_bus #(
 
-    .DEVICE0_START_ADDRESS          (32'h00000000                       ),
-    .DEVICE0_FINAL_ADDRESS          (MEMORY_SIZE-1                      ),
-    .DEVICE1_START_ADDRESS          (32'h80000000                       ),
-    .DEVICE1_FINAL_ADDRESS          (32'h80000004                       )
-
-    /* Uncomment to add new devices
-
-    .DEVICE2_START_ADDRESS          (32'hdeadbeef                       ),
-    .DEVICE2_FINAL_ADDRESS          (32'hdeadbeef                       ),
-    .DEVICE3_START_ADDRESS          (32'hdeadbeef                       ),
-    .DEVICE3_FINAL_ADDRESS          (32'hdeadbeef                       )
-
-    */
+    .NUM_DEVICES(NUM_DEVICES)
 
   ) system_bus_instance (
+
+    // Global signals
 
     .clock                          (clock                              ),
     .reset                          (reset                              ),
 
-    // RISC-V Steel 32-bit Processor (Manager Device) <=> System Bus
+    // Interface with the manager device (Processor Core IP)
 
-    .rw_address                     (rw_address                         ),
-    .read_data                      (read_data                          ),
-    .read_request                   (read_request                       ),
-    .read_response                  (read_response                      ),
-    .write_data                     (write_data                         ),
-    .write_strobe                   (write_strobe                       ),
-    .write_request                  (write_request                      ),
-    .write_response                 (write_response                     ),
+    .manager_rw_address             (manager_rw_address                 ),
+    .manager_read_data              (manager_read_data                  ),
+    .manager_read_request           (manager_read_request               ),
+    .manager_read_response          (manager_read_response              ),
+    .manager_write_data             (manager_write_data                 ),
+    .manager_write_strobe           (manager_write_strobe               ),
+    .manager_write_request          (manager_write_request              ),
+    .manager_write_response         (manager_write_response             ),
 
-    // RAM Memory (Device #0) <=> System Bus
+    // Interface with the managed devices
 
-    .device0_rw_address             (device0_rw_address                 ),
-    .device0_read_data              (device0_read_data                  ),
-    .device0_read_request           (device0_read_request               ),
-    .device0_read_response          (device0_read_response              ),
-    .device0_write_data             (device0_write_data                 ),
-    .device0_write_strobe           (device0_write_strobe               ),
-    .device0_write_request          (device0_write_request              ),
-    .device0_write_response         (device0_write_response             ),
+    .device_rw_address              (device_rw_address                  ),
+    .device_read_data               (device_read_data                   ),
+    .device_read_request            (device_read_request                ),
+    .device_read_response           (device_read_response               ),
+    .device_write_data              (device_write_data                  ),
+    .device_write_strobe            (device_write_strobe                ),
+    .device_write_request           (device_write_request               ),
+    .device_write_response          (device_write_response              ),
 
-    // UART (Device #1) <=> System Bus
+    // Base addresses and masks of the managed devices
 
-    .device1_rw_address             (device1_rw_address                 ),
-    .device1_read_data              (device1_read_data                  ),
-    .device1_read_request           (device1_read_request               ),
-    .device1_read_response          (device1_read_response              ),
-    .device1_write_data             (device1_write_data                 ),
-    .device1_write_strobe           (device1_write_strobe               ),
-    .device1_write_request          (device1_write_request              ),
-    .device1_write_response         (device1_write_response             )
-
-    /* Uncomment to add new devices
-
-    // Device #2 <=> System Bus
-
-    .device2_rw_address             (device2_rw_address                 ),
-    .device2_read_data              (device2_read_data                  ),
-    .device2_read_request           (device2_read_request               ),
-    .device2_read_response          (device2_read_response              ),
-    .device2_write_data             (device2_write_data                 ),
-    .device2_write_strobe           (device2_write_strobe               ),
-    .device2_write_request          (device2_write_request              ),
-    .device2_write_response         (device2_write_response             )
-
-    // Device #3 <=> System Bus
-
-    .device3_rw_address             (device3_rw_address                 ),
-    .device3_read_data              (device3_read_data                  ),
-    .device3_read_request           (device3_read_request               ),
-    .device3_read_response          (device3_read_response              ),
-    .device3_write_data             (device3_write_data                 ),
-    .device3_write_strobe           (device3_write_strobe               ),
-    .device3_write_request          (device3_write_request              ),
-    .device3_write_response         (device3_write_response             )
-
-    */
+    .device_base_address            (device_base_address                ),
+    .device_mask_address            (device_mask_address                )
 
   );
 
@@ -258,14 +192,14 @@ module rvsteel_soc #(
 
     // IO interface
 
-    .rw_address                     (device0_rw_address                 ),
-    .read_data                      (device0_read_data                  ),
-    .read_request                   (device0_read_request               ),
-    .read_response                  (device0_read_response              ),
-    .write_data                     (device0_write_data                 ),
-    .write_strobe                   (device0_write_strobe               ),
-    .write_request                  (device0_write_request              ),
-    .write_response                 (device0_write_response             )
+    .rw_address                     (device_rw_address                  ),
+    .read_data                      (device_read_data[32*D0_RAM +: 32]  ),
+    .read_request                   (device_read_request[D0_RAM]        ),
+    .read_response                  (device_read_response[D0_RAM]       ),
+    .write_data                     (device_write_data                  ),
+    .write_strobe                   (device_write_strobe                ),
+    .write_request                  (device_write_request[D0_RAM]       ),
+    .write_response                 (device_write_response[D0_RAM]      )
 
   );
 
@@ -283,13 +217,13 @@ module rvsteel_soc #(
 
     // IO interface
 
-    .rw_address                     (device1_rw_address                 ),
-    .read_data                      (device1_read_data                  ),
-    .read_request                   (device1_read_request               ),
-    .read_response                  (device1_read_response              ),
-    .write_data                     (device1_write_data[7:0]            ),
-    .write_request                  (device1_write_request              ),
-    .write_response                 (device1_write_response             ),
+    .rw_address                     (device_rw_address                  ),
+    .read_data                      (device_read_data[32*D1_UART +: 32] ),
+    .read_request                   (device_read_request[D1_UART]       ),
+    .read_response                  (device_read_response[D1_UART]      ),
+    .write_data                     (device_write_data[7:0]             ),
+    .write_request                  (device_write_request[D1_UART]      ),
+    .write_response                 (device_write_response[D1_UART]     ),
 
     // RX/TX signals
 
@@ -302,39 +236,5 @@ module rvsteel_soc #(
     .uart_irq_response              (irq_external_response              )
 
   );
-
-  /* Uncomment to add new devices
-
-  mydevice2
-  mydevice2_instance (
-
-    ... device 2 signals ...
-
-    .mydevice2_rw_address           (device2_rw_address                 ),
-    .mydevice2_read_data            (device2_read_data                  ),
-    .mydevice2_read_request         (device2_read_request               ),
-    .mydevice2_read_response        (device2_read_response              ),
-    .mydevice2_write_data           (device2_write_data                 ),
-    .mydevice2_write_request        (device2_write_request              ),
-    .mydevice2_write_response       (device2_write_response             )
-
-  );
-
-  mydevice3
-  mydevice3_instance (
-
-    ... device 3 signals ...
-
-    .mydevice3_rw_address           (device3_rw_address                 ),
-    .mydevice3_read_data            (device3_read_data                  ),
-    .mydevice3_read_request         (device3_read_request               ),
-    .mydevice3_read_response        (device3_read_response              ),
-    .mydevice3_write_data           (device3_write_data                 ),
-    .mydevice3_write_request        (device3_write_request              ),
-    .mydevice3_write_response       (device3_write_response             )
-
-  );
-
-  */
 
 endmodule
