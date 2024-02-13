4a15347e8f483cf18a89e239c5c4785cc7be0394
AlexxMarkov <markov0192@mail.ru>
2024-02-14 00:53:50 +0400

    Added project files

---
+++ software/template_cmake/main.cpp
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2020-2024 RISC-V Steel contributors
+//
+// This work is licensed under the MIT License, see LICENSE file for details.
+// SPDX-License-Identifier: MIT
+// ----------------------------------------------------------------------------
+
+#include "rvsteel_soc.h"
+
+int main(void)
+{
+  uart_send_string(UART0, "Hello World!\n");
+  while (1);
+}
