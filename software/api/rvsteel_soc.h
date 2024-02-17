9347f564b5f697c88fa72f25c1b0b7d71849a685
AlexxMarkov <markov0192@mail.ru>
2024-02-17 23:13:30 +0400

    Added GPIO to rvsteel_soc.h

--- software/api/rvsteel_soc.h
+++ software/api/rvsteel_soc.h
@@ -8,8 +8,10 @@
 #ifndef __RVSTEEL_SOC__
 #define __RVSTEEL_SOC__
 
-#include "rvsteel_mtimer.h"
+#include "rvsteel_csr.h"
 #include "rvsteel_uart.h"
+#include "rvsteel_mtimer.h"
+#include "rvsteel_gpio.h"
 
 // Base address
 #define MAIN_BASE_ADDR 0x00000000U
@@ -17,10 +19,12 @@
 // Map devices
 #define UART0_BASE (MAIN_BASE_ADDR + 0x80000000U)
 #define MTIMER0_BASE (MAIN_BASE_ADDR + 0x80010000U)
+#define GPIO_BASE (MAIN_BASE_ADDR + 0x80020000U)
 
 // List devices
-#define MTIMER0 ((MTimerDevice *)MTIMER0_BASE)
 #define UART0 ((UartDevice *)UART0_BASE)
+#define MTIMER0 ((MTimerDevice *)MTIMER0_BASE)
+#define GPIO0 ((GpioDevice *)UART0_BASE)
 
 // Machine Interrupt mask
 #define IRQ_MTIMER0_MASK MIP_MIE_MASK_MTI
