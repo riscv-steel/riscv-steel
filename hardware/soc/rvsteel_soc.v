0bd06edd18e406d13f7f56e5def3c92d6f38debe
Rafael Calcada <rafaelcalcada@gmail.com>
2024-02-25 21:47:54 +0100

    Fix UART addresses

--- hardware/soc/rvsteel_soc.v
+++ hardware/soc/rvsteel_soc.v
@@ -240,7 +240,7 @@ module rvsteel_soc #(
 
     // IO interface
 
-    .rw_address                     (device_rw_address                  ),
+    .rw_address                     (device_rw_address[4:0]             ),
     .read_data                      (device_read_data[32*D1_UART +: 32] ),
     .read_request                   (device_read_request[D1_UART]       ),
     .read_response                  (device_read_response[D1_UART]      ),
