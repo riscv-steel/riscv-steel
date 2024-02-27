e65d31d1388a38e70dbd6fd4d88b7c4d45c7e1e0
Rafael Calcada <rafaelcalcada@gmail.com>
2024-02-27 22:20:52 +0100

    Fix typo in Cmod A7 Tcl script

--- demos/hello_world/boards/cmod_a7/create_project_cmod_a7.tcl
+++ demos/hello_world/boards/cmod_a7/create_project_cmod_a7.tcl
@@ -4,6 +4,6 @@ set_msg_config -suppress -id {Synth 8-7080}
 set_msg_config -suppress -id {Power 33-332}
 set_msg_config -suppress -id {Pwropt 34-321}
 set_property simulator_language Verilog [current_project]
-add_files -fileset constrs_1 -norecurse { ./hello_world_arty_a7_constraints.xdc }
-add_files -norecurse { ./hello_world_arty_a7.v ../../../../hardware/core/rvsteel_core.v ../../../../hardware/ram/rvsteel_ram.v ../../../../hardware/soc/rvsteel_soc.v ../../../../hardware/bus/rvsteel_bus.v ../../../../hardware/uart/rvsteel_uart.v ../../../../hardware/gpio/rvsteel_gpio.v ../../../../hardware/spi/rvsteel_spi.v ../../../../hardware/mtimer/rvsteel_mtimer.v ../../software/build/hello_world.hex }
+add_files -fileset constrs_1 -norecurse { ./hello_world_cmod_a7_constraints.xdc }
+add_files -norecurse { ./hello_world_cmod_a7.v ../../../../hardware/core/rvsteel_core.v ../../../../hardware/ram/rvsteel_ram.v ../../../../hardware/soc/rvsteel_soc.v ../../../../hardware/bus/rvsteel_bus.v ../../../../hardware/uart/rvsteel_uart.v ../../../../hardware/gpio/rvsteel_gpio.v ../../../../hardware/spi/rvsteel_spi.v ../../../../hardware/mtimer/rvsteel_mtimer.v ../../software/build/hello_world.hex }
 set_property file_type {Memory Initialization Files} [get_files ../../software/build/hello_world.hex]
