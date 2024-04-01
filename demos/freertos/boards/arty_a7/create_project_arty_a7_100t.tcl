17cade656c86f745b892b0a00a759c057cfc8404
Rafael Calcada <rafaelcalcada@gmail.com>
2024-04-01 20:15:00 +0200

    Add FreeRTOS demo for Arty A7. Move Arty and Cmod demos to boards/ folder

---
+++ demos/freertos/boards/arty_a7/create_project_arty_a7_100t.tcl
@@ -0,0 +1,10 @@
+cd [file normalize [file dirname [info script]]]
+create_project freertos_arty_a7_100t ./freertos_arty_a7_100t -part xc7a100ticsg324-1L -force
+set_msg_config -suppress -id {Synth 8-7080}
+set_msg_config -suppress -id {Power 33-332}
+set_msg_config -suppress -id {Pwropt 34-321}
+set_msg_config -suppress -id {Synth 8-6841}
+set_property simulator_language Verilog [current_project]
+add_files -fileset constrs_1 -norecurse { ./freertos_arty_a7_constraints.xdc }
+add_files -norecurse { ./freertos_arty_a7.v ../../../../hardware/core/rvsteel_core.v ../../../../hardware/ram/rvsteel_ram.v ../../../../hardware/soc/rvsteel_soc.v ../../../../hardware/bus/rvsteel_bus.v ../../../../hardware/uart/rvsteel_uart.v ../../../../hardware/gpio/rvsteel_gpio.v ../../../../hardware/spi/rvsteel_spi.v ../../../../hardware/mtimer/rvsteel_mtimer.v ../../software/build/freertos_demo.hex }
+set_property file_type {Memory Initialization Files} [get_files ../../software/build/freertos_demo.hex]
---
+++ demos/freertos/boards/arty_a7/freertos_arty_a7.v
@@ -0,0 +1,59 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2020-2024 RISC-V Steel contributors
+//
+// This work is licensed under the MIT License, see LICENSE file for details.
+// SPDX-License-Identifier: MIT
+// ----------------------------------------------------------------------------
+
+module hello_world_cmod_a7 (
+
+  input   wire clock,
+  input   wire reset,
+  input   wire halt,
+  input   wire uart_rx,
+  output  wire uart_tx,
+  output  wire gpio0,
+  output  wire gpio1
+
+  );
+
+  // Divides the 100MHz board block by 2
+  reg clock_50mhz;
+  initial clock_50mhz = 1'b0;
+  always @(posedge clock) clock_50mhz <= !clock_50mhz;
+
+  // Buttons debouncing
+  reg reset_debounced;
+  reg halt_debounced;
+  always @(posedge clock_50mhz) begin
+    reset_debounced <= reset;
+    halt_debounced <= halt;
+  end
+
+  rvsteel_soc #(
+
+    .CLOCK_FREQUENCY          (12000000               ),
+    .UART_BAUD_RATE           (9600                   ),
+    .MEMORY_SIZE              (131072                 ),
+    .MEMORY_INIT_FILE         ("freertos_demo.hex"    ),
+    .BOOT_ADDRESS             (32'h00000000           ),
+    .GPIO_WIDTH               (2                      )
+
+  ) rvsteel_soc_instance (
+
+    .clock                    (clock_50mhz            ),
+    .reset                    (reset_debounced        ),
+    .halt                     (halt_debounced         ),
+    .uart_rx                  (uart_rx                ),
+    .uart_tx                  (uart_tx                ),
+    .gpio_input               (1'b0                   ),
+    .gpio_oe                  (), // unused
+    .gpio_output              ({gpio1,gpio0}          ),
+    .sclk                     (), // unused
+    .pico                     (), // unused
+    .poci                     (1'b0                   ),
+    .cs                       ()  // unused
+
+  );
+
+endmodule
