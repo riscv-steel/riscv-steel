c4eff9172fc251c88cd09fd39de3c9364a852da8
Rafael Calcada <rafaelcalcada@gmail.com>
2024-04-02 00:25:59 +0200

    Update GPIO demo software

--- demos/gpio/software/main.c
+++ demos/gpio/software/main.c
@@ -5,14 +5,17 @@
 // SPDX-License-Identifier: MIT
 // ----------------------------------------------------------------------------
 
-#include "rvsteel_soc.h"
+#include "libsteel/soc.h"
 
-int main(void)
+void main(void)
 {
-  gpio_set_output(GPIO0, 0);
-
+  gpio_set_output(RVSTEEL_GPIO, 0);
+  gpio_set_output(RVSTEEL_GPIO, 1);
+  gpio_set_input(RVSTEEL_GPIO, 2);
+  gpio_write(RVSTEEL_GPIO, 0, HIGH);
   while (1)
   {
-    gpio_toggle(GPIO0, 0);
+    uint32_t button_state = gpio_read(RVSTEEL_GPIO, 2);
+    gpio_write(RVSTEEL_GPIO, 1, button_state);
   }
 }
