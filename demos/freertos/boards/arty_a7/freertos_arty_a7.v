95e2eee579d4124e76f81e9cf707682caaea4b0d
Rafael Calcada <rafaelcalcada@gmail.com>
2024-04-03 17:12:05 +0200

    Update GPIO usage on FreeRTOS demo

--- demos/freertos/boards/arty_a7/freertos_arty_a7.v
+++ demos/freertos/boards/arty_a7/freertos_arty_a7.v
@@ -5,17 +5,31 @@
 // SPDX-License-Identifier: MIT
 // ----------------------------------------------------------------------------
 
-module freertos_arty_a7 (
+module freertos_arty_a7  #(
+  
+  parameter GPIO_WIDTH = 2
+
+  )(
 
   input   wire clock,
   input   wire reset,
   input   wire halt,
   input   wire uart_rx,
   output  wire uart_tx,
-  output  wire gpio0,
-  output  wire gpio1
+  inout   wire [GPIO_WIDTH-1:0] gpio
 
   );
+
+  // GPIO signals
+  wire [GPIO_WIDTH-1:0] gpio_input;
+  wire [GPIO_WIDTH-1:0] gpio_oe;
+  wire [GPIO_WIDTH-1:0] gpio_output;
+
+  genvar i;
+  for (i = 0; i < GPIO_WIDTH; i=i+1) begin
+    assign gpio_input[i] = gpio_oe[i] == 1'b1 ? gpio_output[i] : gpio[i];
+    assign gpio[i] = gpio_oe[i] == 1'b1 ? gpio_output[i] : 1'bZ;
+  end
 
   // Divides the 100MHz board block by 2
   reg clock_50mhz;
@@ -46,9 +60,9 @@ module freertos_arty_a7 (
     .halt                     (halt_debounced         ),
     .uart_rx                  (uart_rx                ),
     .uart_tx                  (uart_tx                ),
-    .gpio_input               (1'b0                   ),
-    .gpio_oe                  (), // unused
-    .gpio_output              ({gpio1,gpio0}          ),
+    .gpio_input               (gpio_input             ),
+    .gpio_oe                  (gpio_oe                ),
+    .gpio_output              (gpio_output            ),
     .sclk                     (), // unused
     .pico                     (), // unused
     .poci                     (1'b0                   ),
