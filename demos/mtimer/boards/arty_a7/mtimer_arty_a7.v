8689e9dff9921bc5b84552f9725ce6a972ad7903
Rafael Calcada <rafaelcalcada@gmail.com>
2024-04-14 21:04:52 +0200

    Add MTimer demo for Arty and Cmod

---
+++ demos/mtimer/boards/arty_a7/mtimer_arty_a7.v
@@ -0,0 +1,70 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2020-2024 RISC-V Steel contributors
+//
+// This work is licensed under the MIT License, see LICENSE file for details.
+// SPDX-License-Identifier: MIT
+// ----------------------------------------------------------------------------
+
+module mtimer_arty_a7 #( 
+
+  parameter GPIO_WIDTH = 3
+
+  )(
+
+  input   wire clock,
+  input   wire reset,
+  input   wire uart_rx,
+  output  wire uart_tx,
+  inout   wire [GPIO_WIDTH-1:0] gpio
+
+  );
+  
+  // GPIO signals
+  wire [GPIO_WIDTH-1:0] gpio_input;
+  wire [GPIO_WIDTH-1:0] gpio_oe;
+  wire [GPIO_WIDTH-1:0] gpio_output;
+
+  genvar i;
+  for (i = 0; i < GPIO_WIDTH; i=i+1) begin
+    assign gpio_input[i] = gpio_oe[i] == 1'b1 ? gpio_output[i] : gpio[i];
+    assign gpio[i] = gpio_oe[i] == 1'b1 ? gpio_output[i] : 1'bZ;
+  end
+
+  // Divides the 100MHz board block by 2
+  reg clock_50mhz;
+  initial clock_50mhz = 1'b0;
+  always @(posedge clock) clock_50mhz <= !clock_50mhz;
+
+  // Buttons debouncing
+  reg reset_debounced;
+  always @(posedge clock_50mhz) begin
+    reset_debounced <= reset;
+  end
+
+  rvsteel_soc #(
+
+    .CLOCK_FREQUENCY          (50000000               ),
+    .UART_BAUD_RATE           (9600                   ),
+    .MEMORY_SIZE              (131072                 ),
+    .MEMORY_INIT_FILE         ("mtimer_demo.hex"      ),
+    .BOOT_ADDRESS             (32'h00000000           ),
+    .GPIO_WIDTH               (3                      )
+
+  ) rvsteel_soc_instance (
+
+    .clock                    (clock_50mhz            ),
+    .reset                    (reset_debounced        ),
+    .halt                     (1'b0                   ),
+    .uart_rx                  (uart_rx                ),
+    .uart_tx                  (uart_tx                ),
+    .gpio_input               (gpio_input             ),
+    .gpio_oe                  (gpio_oe                ),
+    .gpio_output              (gpio_output            ),
+    .sclk                     (), // unused
+    .pico                     (), // unused
+    .poci                     (1'b0                   ),
+    .cs                       ()  // unused
+
+  );
+
+endmodule
---
+++ demos/mtimer/software/main.c
@@ -0,0 +1,29 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) 2020-2024 RISC-V Steel contributors
+//
+// This work is licensed under the MIT License, see LICENSE file for details.
+// SPDX-License-Identifier: MIT
+// ----------------------------------------------------------------------------
+
+#include "libsteel/soc.h"
+
+// Overrides the standard interrupt handler for Machine Timer interrupts
+__NAKED void mti_irq_handler()
+{
+  mtimer_clear_counter(RVSTEEL_MTIMER);
+  uart_write_string(RVSTEEL_UART, "Time elapsed: 1 sec\n");
+  __ASM_VOLATILE("mret");
+}
+
+int main(void)
+{
+  uart_write_string(RVSTEEL_UART, "MTimer demo started running...\n");
+  mtimer_set_compare(RVSTEEL_MTIMER, CPU_FREQUENCY);
+  mtimer_clear_counter(RVSTEEL_MTIMER);
+  mtimer_enable(RVSTEEL_MTIMER);
+  csr_enable_vectored_mode_irq();
+  CSR_SET(CSR_MIE, MIP_MIE_MASK_MTI);
+  csr_global_enable_irq();
+  while (1)
+    ;
+}
