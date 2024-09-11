ff1bb9b04650446e1556e2dc870d210df8d66df5
Rafael Calcada <rafaelcalcada@gmail.com>
2024-09-11 22:01:00 +0200

    Update GPIO example

--- examples/gpio/software/main.c
+++ examples/gpio/software/main.c
@@ -7,15 +7,17 @@
 
 #include "libsteel.h"
 
+#define GPIO_CONTROLLER_ADDR (GpioController *)0x80020000
+
 void main(void)
 {
-  gpio_set_output(RVSTEEL_GPIO, 0);
-  gpio_set_output(RVSTEEL_GPIO, 1);
-  gpio_set_input(RVSTEEL_GPIO, 2);
-  gpio_write(RVSTEEL_GPIO, 0, HIGH);
+  gpio_set_output(GPIO_CONTROLLER_ADDR, 0);
+  gpio_set_output(GPIO_CONTROLLER_ADDR, 1);
+  gpio_set_input(GPIO_CONTROLLER_ADDR, 2);
+  gpio_write(GPIO_CONTROLLER_ADDR, 0, HIGH);
   while (1)
   {
-    uint32_t button_state = gpio_read(RVSTEEL_GPIO, 2);
-    gpio_write(RVSTEEL_GPIO, 1, button_state);
+    uint32_t button_state = gpio_read(GPIO_CONTROLLER_ADDR, 2);
+    gpio_write(GPIO_CONTROLLER_ADDR, 1, button_state);
   }
 }
