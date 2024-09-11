e67e7d52a83485ef3673cef0b5f8ccd9b6296163
Rafael Calcada <rafaelcalcada@gmail.com>
2024-09-12 00:14:43 +0200

    Update UART example

--- examples/uart/software/main.c
+++ examples/uart/software/main.c
@@ -7,21 +7,23 @@
 
 #include "libsteel.h"
 
+#define UART_CONTROLLER_ADDR (UartController *)0x80000000
+
 // UART interrupt signal is connected to Fast IRQ #0
 __NAKED void fast0_irq_handler(void)
 {
-  char rx = uart_read(RVSTEEL_UART);
+  char rx = uart_read(UART_CONTROLLER_ADDR);
   if (rx == '\r') // Enter key
-    uart_write_string(RVSTEEL_UART, "\n\nType something else and press enter: ");
+    uart_write_string(UART_CONTROLLER_ADDR, "\n\nType something else and press enter: ");
   else if (rx < 127)
-    uart_write(RVSTEEL_UART, rx);
+    uart_write(UART_CONTROLLER_ADDR, rx);
   __ASM_VOLATILE("mret");
 }
 
 void main(void)
 {
-  uart_write_string(RVSTEEL_UART, "RISC-V Steel - UART demo");
-  uart_write_string(RVSTEEL_UART, "\n\nType something and press Enter:\n");
+  uart_write_string(UART_CONTROLLER_ADDR, "RISC-V Steel - UART demo");
+  uart_write_string(UART_CONTROLLER_ADDR, "\n\nType something and press Enter:\n");
   csr_enable_vectored_mode_irq();
   CSR_SET(CSR_MIE, MIP_MIE_MASK_F0I);
   csr_global_enable_irq();
