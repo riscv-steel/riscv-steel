63424f14e8b92b140c7dc723340db09d73d7fc4d
Rafael Calcada <rafaelcalcada@gmail.com>
2024-09-22 01:05:30 +0200

    Update default LibSteel version

--- examples/freertos/software/LibSteel.cmake
+++ examples/freertos/software/LibSteel.cmake
@@ -9,7 +9,7 @@ include(FetchContent)
 
 FetchContent_Declare(steel
   GIT_REPOSITORY https://github.com/riscv-steel/libsteel.git
-  GIT_TAG v1.2
+  GIT_TAG v1.3
 )
 
 FetchContent_MakeAvailable(steel)
