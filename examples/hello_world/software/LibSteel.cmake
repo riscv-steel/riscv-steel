454cbcdc3335dd0d76eb5594685ba82143154fd8
Rafael Calcada <rafaelcalcada@gmail.com>
2024-09-22 01:05:30 +0200

    Update default LibSteel version

--- examples/hello_world/software/LibSteel.cmake
+++ examples/hello_world/software/LibSteel.cmake
@@ -5,11 +5,11 @@
 # SPDX-License-Identifier: MIT
 # -----------------------------------------------------------------------------
 
-include (FetchContent)
+include(FetchContent)
 
 FetchContent_Declare(steel
   GIT_REPOSITORY https://github.com/riscv-steel/libsteel.git
-  GIT_TAG        v1.2
+  GIT_TAG v1.3
 )
 
 FetchContent_MakeAvailable(steel)
