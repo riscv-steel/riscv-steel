02ebcd4979864bd84453e777479fe39058f4fa75
Rafael Calcada <rafaelcalcada@gmail.com>
2024-09-22 00:56:39 +0200

    Update UART example

--- examples/uart/software/main.c
+++ examples/uart/software/main.c
@@ -9,24 +9,19 @@
 
 #define UART_CONTROLLER_ADDR (UartController *)0x80000000
 
-// UART interrupt signal is connected to Fast IRQ #0
-__NAKED void fast0_irq_handler(void)
-{
-  char rx = uart_read(UART_CONTROLLER_ADDR);
-  if (rx == '\r') // Enter key
-    uart_write_string(UART_CONTROLLER_ADDR, "\n\nType something else and press enter: ");
-  else if (rx < 127)
-    uart_write(UART_CONTROLLER_ADDR, rx);
-  __ASM_VOLATILE("mret");
-}
-
 void main(void)
 {
-  uart_write_string(UART_CONTROLLER_ADDR, "RISC-V Steel - UART demo");
-  uart_write_string(UART_CONTROLLER_ADDR, "\n\nType something and press Enter:\n");
-  csr_enable_vectored_mode_irq();
-  CSR_SET(CSR_MIE, MIP_MIE_MASK_F0I);
-  csr_global_enable_irq();
+  uart_write_string(UART_CONTROLLER_ADDR, "RISC-V Steel - UART example"
+                                          "\n\nType something and press Enter:\n");
   while (1)
-    ;
+  {
+    if (uart_data_received(UART_CONTROLLER_ADDR))
+    {
+      char rx = uart_read(UART_CONTROLLER_ADDR);
+      if (rx == '\r') // Enter key
+        uart_write_string(UART_CONTROLLER_ADDR, "\n\nType something else and press Enter again: ");
+      else if (rx < 127) // Echo back printable characters
+        uart_write(UART_CONTROLLER_ADDR, rx);
+    }
+  }
 }
