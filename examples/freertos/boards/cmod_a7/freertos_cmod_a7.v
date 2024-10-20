ce382863733bea470af4bbf474c41d33379f20ae
Rafael Calcada <rafaelcalcada@gmail.com>
2024-10-20 21:03:57 +0200

    Fix more examples

--- examples/freertos/boards/cmod_a7/freertos_cmod_a7.v
+++ examples/freertos/boards/cmod_a7/freertos_cmod_a7.v
@@ -43,8 +43,8 @@ module freertos_cmod_a7 #(
 
     .CLOCK_FREQUENCY          (12000000               ),
     .UART_BAUD_RATE           (9600                   ),
-    .MEMORY_SIZE              (131072                 ),
-    .MEMORY_INIT_FILE         ("freertos.hex"    ),
+    .MEMORY_SIZE              (32768                  ),
+    .MEMORY_INIT_FILE         ("freertos.hex"         ),
     .BOOT_ADDRESS             (32'h00000000           ),
     .GPIO_WIDTH               (2                      )
 
