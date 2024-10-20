15c484f83b142347bdfcfa7d65dcde81a6924346
Rafael Calcada <rafaelcalcada@gmail.com>
2024-10-20 21:12:45 +0200

    More small fixes

--- examples/mtimer/boards/arty_a7/mtimer_arty_a7.v
+++ examples/mtimer/boards/arty_a7/mtimer_arty_a7.v
@@ -45,7 +45,7 @@ module mtimer_arty_a7 #(
 
     .CLOCK_FREQUENCY          (50000000               ),
     .UART_BAUD_RATE           (9600                   ),
-    .MEMORY_SIZE              (131072                 ),
+    .MEMORY_SIZE              (8192                   ),
     .MEMORY_INIT_FILE         ("mtimer_demo.hex"      ),
     .BOOT_ADDRESS             (32'h00000000           ),
     .GPIO_WIDTH               (3                      )
