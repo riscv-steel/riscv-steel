2f5105888527282a450980f2afa4ecbc43f2d18b
Rafael Calcada <rafaelcalcada@gmail.com>
2024-10-20 21:40:19 +0200

    More fixes [2]

--- examples/spi/boards/arty_a7/spi_arty_a7.v
+++ examples/spi/boards/arty_a7/spi_arty_a7.v
@@ -38,7 +38,7 @@ module spi_arty_a7 #(
 
     .CLOCK_FREQUENCY          (50000000               ),
     .UART_BAUD_RATE           (9600                   ),
-    .MEMORY_SIZE              (131072                 ),
+    .MEMORY_SIZE              (8192                   ),
     .MEMORY_INIT_FILE         ("spi_demo.hex"         ),
     .BOOT_ADDRESS             (32'h00000000           ),
     .GPIO_WIDTH               (GPIO_WIDTH             ),
